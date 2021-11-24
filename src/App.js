e7a81b116fd1172bc7fad83ccadba37fb8d950aa
laura <ll.calderon14@gmail.com>
2021-11-24 12:05:00 -0500

    install Axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState , useEffect} from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from "react-bootstrap";
+import axios from "axios";
+import {API_URL} from "./constants/index.js"
 import './App.scss';
 import CardContainer from "./components/CardContainer/index.js";
 
@@ -9,25 +11,30 @@ import CardContainer from "./components/CardContainer/index.js";
 function App() {
 
   const[cars, setCars]= useState([]);
+  
+const getCarAxios =()=>{
+  const endpoint = "/api/car"
+   axios.get(`${API_URL}${endpoint}`)
+   .then(result =>{
+    console.log("data axios",result)
+    setCars(result.data)
 
-  useEffect(()=>{
-    const loadData =()=>{
-      fetch("https://api-rental-cars.herokuapp.com/api/car")
-      .then(data=>data.json())
-      .then(data =>{
-        console.log("DATA",data)
-        setCars(data)
-      })
-    };
-    loadData();
-  },[]);
+   })
+   .catch (err=>{
+     console.log("error",err)
+   })
+   
+ }
 
+useEffect(()=>{
+  getCarAxios();
+
+},[])
 
   return (
     <Container fluid className="App">
     <h1>Alquila facilmente nuestros autos</h1>
     <CardContainer items={cars} />
-  
     </Container>
   );
 }
