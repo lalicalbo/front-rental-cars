b73a3c067e9fa440e6b40d1c87c101ad066634a1
laura <ll.calderon14@gmail.com>
2021-12-02 14:20:33 -0500

    refactoring

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,10 +1,9 @@
-
 import React from 'react'
 
 const PageNotFound = () => (
   <section className="PageNotFound">
     <h3>Page Not Found 404 :/</h3>
   </section>
-)
+);
 
-export default PageNotFound
+export default PageNotFound;
